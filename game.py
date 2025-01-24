ef79ce118600ce101793338d17fa2cf676d6b9e1
Alexandre Schönwitz <95051128@mail.schwarz>
2025-01-24 20:06:18 +0000

    fix

--- game.py
+++ game.py
@@ -20,7 +20,7 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.circle(screen, "orange", player_pos, 40)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:

ac0ebfb6c732498fb93ef9f5fa321020aa2e27a0
Miguel Crespo <miguel.crespo@freiheit.com>
2025-01-24 21:28:43 +0000

    cannons and bubbles

--- game.py
+++ game.py
@@ -1,5 +1,49 @@
 import pygame
 
+
+# x
+# 0
+# +
+# v
+# 0
+# x
+# t
+# +
+# 1
+# 2
+# a
+# x
+# t
+# 2
+# v² = u² + 2as.
+# s = ut
+
+class Bubble:
+    def __init__(self, init_x, init_y, vel_x, vel_y):
+        self.player_pos = pygame.Vector2(init_x, init_y)
+        self.player_vel = pygame.Vector2(vel_x, vel_y)
+        self.radius = 1
+        self.visible = False
+        self.draw()
+
+    def draw(self):
+        if self.visible:
+            pygame.draw.circle(screen, "orange", self.player_pos, self.radius)
+
+    def tick(self):
+        self.player_pos += self.player_vel * dt
+        self.draw()
+
+    def increase_radius(self):
+        self.radius += 10/self.radius
+
+    def set_visible(self):
+        self.visible = True
+
+    def set_x_vel(self, new_x_vel):
+        self.player_vel.x = new_x_vel
+
+# pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
@@ -18,6 +62,12 @@ player_1_color = (255, 0, 0)
 # Player 2
 player_2_y = screen.get_height() / 2 - cannon_height / 2
 player_2_color = (0, 255, 0)
+
+b = Bubble(screen.get_width() / 2, screen.get_height() / 2, 100, 0)
+pygame.key.set_repeat()
+
+bubbles = [b]
+player_a_bubble = None
 
 while running:
     for event in pygame.event.get():
@@ -41,6 +91,21 @@ while running:
     if keys[pygame.K_DOWN]:
         player_2_y = min(screen.get_height() - cannon_height - padding, player_2_y + player_speed * dt)
 
+    if keys[pygame.K_SPACE]:
+        if player_a_bubble is None:
+            player_a_bubble = Bubble(screen.get_width() / 2, screen.get_height() / 2, 0, 0)
+        else:
+            player_a_bubble.increase_radius()
+            player_a_bubble.set_visible()
+            player_a_bubble.draw()
+            player_a_bubble.increase_radius()
+    else:
+        if player_a_bubble is not None:
+            player_a_bubble.set_x_vel(100)
+            bubbles.append(player_a_bubble)
+        player_a_bubble = None
+    for b in bubbles:
+        b.tick()
     # flip() the display to put your work on screen
     pygame.display.flip()
 

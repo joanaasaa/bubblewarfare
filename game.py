8f546065cb98111ef3c144e7c726ad90ae00def0
Alexandre Schönwitz <95051128@mail.schwarz>
2025-01-24 21:12:24 +0000

    add padding

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+
+padding = 20
 cannon_height = 40
 cannon_width = 40
 dt = 0
@@ -23,20 +25,20 @@ while running:
 
     screen.fill("purple")
 
-    pygame.draw.rect(screen, player_1_color, [20, 10 + player_1_y, 40, cannon_height])
-    pygame.draw.rect(screen, player_2_color, [screen.get_width() - 20 - cannon_width, 10 + player_2_y, 40, cannon_height])
+    pygame.draw.rect(screen, player_1_color, [padding, 10 + player_1_y, 40, cannon_height])
+    pygame.draw.rect(screen, player_2_color, [screen.get_width() - padding - cannon_width, 10 + player_2_y, 40, cannon_height])
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_1_y = max(0, player_1_y - 300 * dt)
     if keys[pygame.K_s]:
-        player_1_y = min(screen.get_height() - cannon_height, player_1_y + 300 * dt)
+        player_1_y = min(screen.get_height() - cannon_height - padding, player_1_y + 300 * dt)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
         player_2_y = max(0, player_2_y - 300 * dt)
     if keys[pygame.K_DOWN]:
-        player_2_y = min(screen.get_height() - cannon_height, player_2_y + 300 * dt)
+        player_2_y = min(screen.get_height() - cannon_height - padding, player_2_y + 300 * dt)
 
     # flip() the display to put your work on screen
     pygame.display.flip()

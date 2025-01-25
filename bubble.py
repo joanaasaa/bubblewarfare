f4a5ccac586f25454d066514425c092ed328f680
Alexandre Schönwitz <95051128@mail.schwarz>
2025-01-25 11:57:56 +0000

    files

---
+++ bubble.py
@@ -0,0 +1,45 @@
+import pygame
+from typing import List
+
+
+class Bubble:
+    def __init__(self, init_x, init_y, vel_x, vel_y, sprite: List[pygame.Surface]):
+        self.pos = pygame.Vector2(init_x, init_y)
+        self.vel = pygame.Vector2(vel_x, vel_y)
+        self.radius: float = 1
+        self.visible = False
+        self.sprites: List[pygame.Surface] = sprite
+        self.currentSprite: int = 0
+        self.sprite_dt = 0
+
+    def draw(self, screen):
+        if self.visible:
+            surface = pygame.transform.scale_by(
+                self.sprites[self.currentSprite], self.radius / 50
+            )
+            rect = surface.get_rect(center=(self.pos))
+            screen.blit(surface, rect)
+            # pygame.draw.circle(screen, "black", self.player_pos, self.radius)
+
+    def tick(self, dt: float, screen):
+        self.pos += self.vel * dt
+        self.sprite_dt += dt
+        if self.sprite_dt > 0.05:
+            self.sprite_dt = 0
+            self.currentSprite = (self.currentSprite + 1) % len(self.sprites)
+        self.draw(screen)
+
+    def increase_radius(self):
+        self.radius += 10 / self.radius
+
+    def set_visible(self):
+        self.visible = True
+
+    def set_x_vel(self, new_x_vel):
+        self.vel.x = new_x_vel
+
+    def momentum(self):
+        return self.vel * self.mass()
+
+    def mass(self):
+        return self.radius**2
--- sprites.py
+++ sprites.py
@@ -1,19 +1,29 @@
 import pygame as pg
 from typing import List
 
+
 class Sprites:
     def __init__(self) -> None:
         # Load bubble sprites
-        bubbleSprites : pg.Surface = pg.image.load("assets/images/bubble.png")
-        bubbleSprite1 : pg.Surface = clip(bubbleSprites, 0, 0, 100, 100)
-        bubbleSprite2 : pg.Surface = clip(bubbleSprites, 100, 0, 100, 100)
-        bubbleSprite3 : pg.Surface = clip(bubbleSprites, 200, 0, 100, 100)
-        bubbleSprite4 : pg.Surface = clip(bubbleSprites, 300, 0, 100, 100)
-        bubbleSprite5 : pg.Surface = clip(bubbleSprites, 400, 0, 100, 100)
-        bubbleSprite6 : pg.Surface = clip(bubbleSprites, 500, 0, 100, 100)
-        bubbleSprite7 : pg.Surface = clip(bubbleSprites, 600, 0, 100, 100)
-        bubbleSprite8 : pg.Surface = clip(bubbleSprites, 600, 0, 100, 100)
-        self.bubble : List[pg.Surface] = [bubbleSprite1, bubbleSprite2, bubbleSprite3, bubbleSprite4, bubbleSprite5, bubbleSprite6, bubbleSprite7, bubbleSprite8]
+        bubbleSprites: pg.Surface = pg.image.load("assets/images/bubble.png")
+        bubbleSprite1: pg.Surface = clip(bubbleSprites, 0, 0, 100, 100)
+        bubbleSprite2: pg.Surface = clip(bubbleSprites, 100, 0, 100, 100)
+        bubbleSprite3: pg.Surface = clip(bubbleSprites, 200, 0, 100, 100)
+        bubbleSprite4: pg.Surface = clip(bubbleSprites, 300, 0, 100, 100)
+        bubbleSprite5: pg.Surface = clip(bubbleSprites, 400, 0, 100, 100)
+        bubbleSprite6: pg.Surface = clip(bubbleSprites, 500, 0, 100, 100)
+        bubbleSprite7: pg.Surface = clip(bubbleSprites, 600, 0, 100, 100)
+        bubbleSprite8: pg.Surface = clip(bubbleSprites, 600, 0, 100, 100)
+        self.bubble: List[pg.Surface] = [
+            bubbleSprite1,
+            bubbleSprite2,
+            bubbleSprite3,
+            bubbleSprite4,
+            bubbleSprite5,
+            bubbleSprite6,
+            bubbleSprite7,
+            bubbleSprite8,
+        ]
 
 
 def clip(surface: pg.Surface, x: int, y: int, x_size: int, y_size: int) -> pg.Surface:
@@ -22,5 +32,3 @@ def clip(surface: pg.Surface, x: int, y: int, x_size: int, y_size: int) -> pg.Su
     handle_surface.set_clip(clipRect)
     image = surface.subsurface(handle_surface.get_clip())
     return image.copy()
-
-
